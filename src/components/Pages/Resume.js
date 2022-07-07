5815d4238684cef086a922ebd3399e002e78e94b
Archit Jain <archit.jain@tufts.edu>
2022-07-07 03:32:35 -0400

    image name fix

---
+++ src/components/Pages/Resume.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Resume.css'
+
+export function Resume() {
+    return (
+        <button className='resume'>Resume</button>
+    )
+}
---
+++ src/components/Send.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Send.css'
+
+
+export function Send() {
+    return (
+        <button className='send'>
+            Send <i className='fa-solid fa-paper-plane' />
+        </button>
+    )
+}
